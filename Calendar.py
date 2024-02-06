43401a4336baed7b7aebe108eb5291f888f57ffb
Jian Lv <lvjian700@gmail.com>
2024-02-06 16:13:32 +1100

    Support booking event with relevent date

--- Calendar.py
+++ Calendar.py
@@ -18,8 +18,11 @@ print(f"Timezone: {current_timezone}");
 from datetime import datetime, timedelta
 import pytz
 
+def tz_now():
+    return pytz.timezone(current_timezone).localize(datetime.now())
+
 def get_time_range(period):
-    now = pytz.timezone(current_timezone).localize(datetime.now())
+    now = tz_now()
     if period == 'today':
         start_of_day = now.replace(hour=0, minute=0, second=0, microsecond=0)
         end_of_day = now.replace(hour=23, minute=59, second=59, microsecond=999999)
@@ -35,11 +38,11 @@ def get_time_range(period):
 def time_to_iso_str(date_time):
     return date_time.isoformat()
 
-def time_to_datetime(time_of_today):
-    # Get the current date
-    now = pytz.timezone(current_timezone).localize(datetime.now())
-    combined_str = f"{now.strftime('%Y-%m-%d')} {time_of_today}"
-    return pytz.timezone(current_timezone).localize(datetime.strptime(combined_str, "%Y-%m-%d %H:%M"))
+# def time_to_datetime(time_of_today):
+#     # Get the current date
+#     now = pytz.timezone(current_timezone).localize(datetime.now())
+#     combined_str = f"{now.strftime('%Y-%m-%d')} {time_of_today}"
+#     return pytz.timezone(current_timezone).localize(datetime.strptime(combined_str, "%Y-%m-%d %H:%M"))
 
 def get_events(period):
     # Set time range based on the period
